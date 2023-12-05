31e5a36e03fecf085e93bc3403c013ce2b294713
Leovany <348083098@qq.com>
2023-12-06 00:35:30 +0800

    增加组件库Vant
    整合组件库Vant ： npm i @vant/auto-import-resolver unplugin-vue-components -D

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,17 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
+// Vant4
+import Components from 'unplugin-vue-components/vite';
+import { VantResolver } from '@vant/auto-import-resolver';
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [vue()],
+  plugins: [
+      vue(),
+      // Vant4
+      Components({
+          resolvers: [VantResolver()],
+      }),
+  ],
 })
+
