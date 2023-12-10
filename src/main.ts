b9bee7b6ab45bc38b2444fc07176abb367f3705d
Leovany <348083098@qq.com>
2023-12-10 09:41:24 +0800

    引入Vant组件库
    npm i vant

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+// 引入Vant组件
+import { Button } from 'vant';
 
-createApp(App).mount('#app')
+var app = createApp(App);
+// 使用Vant组件
+app.use(Button);
+app.mount('#app')
