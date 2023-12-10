35a4c6506a1e77cf7a77cd33d56adecb19ba4c40
Leovany <348083098@qq.com>
2023-12-10 12:36:52 +0800

    引入Vant组件和样式

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,24 @@
-import { createApp } from 'vue'
-import './style.css'
+import {createApp} from 'vue'
 import App from './App.vue'
-// 引入Vant组件
-import { Button } from 'vant';
+// 1. Vant
+//  1.1 引入Vant样式
+import 'vant/lib/index.css';
+//  1.2 引入Vant组件
+import {Button, Icon} from 'vant';
+import {NavBar} from 'vant';
+import {Tabbar, TabbarItem} from 'vant';
+import {Toast} from 'vant';
 
 var app = createApp(App);
 // 使用Vant组件
 app.use(Button);
+// Navbar导航栏
+app.use(NavBar);
+app.use(Icon);
+// Tabbar标签栏
+app.use(Tabbar);
+app.use(TabbarItem);
+// Toast 轻提示
+app.use(Toast);
+
 app.mount('#app')
