cf4637353d1c7e908e0092b270c0f57fa22208a8
Leovany <348083098@qq.com>
2023-12-11 22:59:40 +0800

    增加路由 VueRouter
    1. 安装路由命令 npm install vue-router@4
    2. 安装文档：https://cn.vuejs.org/guide/scaling-up/routing.html

--- src/layouts/BasicLayout.vue
+++ src/layouts/BasicLayout.vue
@@ -5,36 +5,21 @@
       <van-icon name="search" size="18"/>
     </template>
   </van-nav-bar>
-  <!-- 中间内容  -->
-  <div id="content">
-    <template v-if="active === 'index'">
-      <Index/>
-    </template>
+  <!-- 中间内容：通过路由动态加载  -->
+  <router-view/>
 
-    <template v-if="active === 'team'">
-      <Team/>
-    </template>
-
-    <template v-if="active === 'user'">
-      <User/>
-    </template>
-
-  </div>
   <!-- 底部标签栏 -->
-  <van-tabbar v-model="active" @change="onChange">
-    <van-tabbar-item name="index" icon="home-o">主页</van-tabbar-item>
-    <van-tabbar-item name="team" icon="search">队伍</van-tabbar-item>
-    <van-tabbar-item name="user" icon="friends-o">个人</van-tabbar-item>
+  <!--  <van-tabbar v-model="active" @change="onChange" >-->
+  <van-tabbar route @change="onChange">
+    <van-tabbar-item to="/" icon="home-o">主页</van-tabbar-item>
+    <van-tabbar-item to="/team" icon="search">队伍</van-tabbar-item>
+    <van-tabbar-item to="/user" icon="friends-o">个人</van-tabbar-item>
   </van-tabbar>
 
 </template>
 
 <script setup lang="ts">
-import {ref} from "vue";
 import {showToast} from "vant";
-import Index from "../pages/Index.vue";
-import Team from "../pages/Team.vue";
-import User from "../pages/User.vue";
 
 const onClickLeft = () => {
   alert("左")
@@ -43,8 +28,6 @@ const onClickRight = () => {
   alert("右")
 };
 
-const active = ref('index');
-// const onChange = (index) => showToast(`标签 ${index}`);
 const onChange = (index) => {
   showToast(`标签 ${index}`);
 
--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,10 @@
 import {createApp} from 'vue'
 import App from './App.vue'
+
+// 引入路由
+import * as VueRouter from 'vue-router';
+import routes from './config/routes.ts'
+
 // 1. Vant
 //  1.1 引入Vant样式
 import 'vant/lib/index.css';
@@ -9,8 +14,9 @@ import {NavBar} from 'vant';
 import {Tabbar, TabbarItem} from 'vant';
 import {Toast} from 'vant';
 
+
 var app = createApp(App);
-// 使用Vant组件
+// 1. 使用Vant组件
 app.use(Button);
 // Navbar导航栏
 app.use(NavBar);
@@ -20,5 +26,15 @@ app.use(Tabbar);
 app.use(TabbarItem);
 // Toast 轻提示
 app.use(Toast);
+
+// 2. 路由
+const router = VueRouter.createRouter({
+    // 4. 内部提供了 history 模式的实现。为了简单起见，我们在这里使用 hash 模式。
+    history: VueRouter.createWebHashHistory(),
+    routes, // `routes: routes` 的缩写
+})
+//整个应用支持路由。
+app.use(router)
+
 
 app.mount('#app')
