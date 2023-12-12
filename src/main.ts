6a0328ce81c53f927b9333189443a685719f9438
Leovany <348083098@qq.com>
2023-12-12 22:45:31 +0800

    组件按需引入，改成全部引入

--- src/main.ts
+++ src/main.ts
@@ -5,27 +5,14 @@ import App from './App.vue'
 import * as VueRouter from 'vue-router';
 import routes from './config/routes.ts'
 
-// 1. Vant
-//  1.1 引入Vant样式
+// Vant
+//  引入Vant样式
 import 'vant/lib/index.css';
-//  1.2 引入Vant组件
-import {Button, Icon} from 'vant';
-import {NavBar} from 'vant';
-import {Tabbar, TabbarItem} from 'vant';
-import {Toast} from 'vant';
-
+import Vant from 'vant';
 
 var app = createApp(App);
-// 1. 使用Vant组件
-app.use(Button);
-// Navbar导航栏
-app.use(NavBar);
-app.use(Icon);
-// Tabbar标签栏
-app.use(Tabbar);
-app.use(TabbarItem);
-// Toast 轻提示
-app.use(Toast);
+// 1. 组件
+app.use(Vant);
 
 // 2. 路由
 const router = VueRouter.createRouter({
