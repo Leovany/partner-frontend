944458b2b3ee71d03e591c28a5393cdd6b8b8f1d
Leovany <348083098@qq.com>
2023-12-19 23:17:31 +0800

    封装卡片组件

---
+++ src/components/UserCardList.vue
@@ -0,0 +1,39 @@
+<template>
+  <!-- 组件内容 -->
+  <van-card
+      v-for="user in props.userList"
+      :title="`${user.username} ( ${user.planetCode} )`"
+      :desc="user.profile"
+      :thumb="user.avatarUrl"
+      tag="VIP"
+  >
+    <template #tags>
+      <template v-for="tag in user.tags">
+        <van-tag plain type="primary" style="margin: 10px 8px 0px 0px">{{ tag }}</van-tag>
+      </template>
+    </template>
+    <template #footer>
+      <van-button size="mini">联系我</van-button>
+    </template>
+  </van-card>
+</template>
+
+<script setup lang="ts">
+import {UserType} from "../models/user";
+
+// 定义向外暴露的组件
+interface UserCardListProps {
+  userList:UserType[],
+}
+
+const props = withDefaults(defineProps<UserCardListProps>(),{
+  // 设置默认值
+  userList : [],
+});
+
+</script>
+
+
+<style scoped>
+
+</style>
--- src/pages/SearchResultPage.vue
+++ src/pages/SearchResultPage.vue
@@ -1,20 +1,22 @@
 <template>
-  <van-card
-      v-for="user in userList"
-      :title="`${user.username} ( ${user.planetCode} )`"
-      :desc="user.profile"
-      :thumb="user.avatarUrl"
-      tag="VIP"
-  >
-    <template #tags>
-      <template v-for="tag in user.tags">
-        <van-tag plain type="primary" style="margin: 10px 8px 0px 0px">{{ tag }}</van-tag>
-      </template>
-    </template>
-    <template #footer>
-      <van-button size="mini">联系我</van-button>
-    </template>
-  </van-card>
+<!--  <van-card-->
+<!--      v-for="user in userList"-->
+<!--      :title="`${user.username} ( ${user.planetCode} )`"-->
+<!--      :desc="user.profile"-->
+<!--      :thumb="user.avatarUrl"-->
+<!--      tag="VIP"-->
+<!--  >-->
+<!--    <template #tags>-->
+<!--      <template v-for="tag in user.tags">-->
+<!--        <van-tag plain type="primary" style="margin: 10px 8px 0px 0px">{{ tag }}</van-tag>-->
+<!--      </template>-->
+<!--    </template>-->
+<!--    <template #footer>-->
+<!--      <van-button size="mini">联系我</van-button>-->
+<!--    </template>-->
+<!--  </van-card>-->
+  <!-- 组件 -->
+  <UserCardList :user-list="userList" />
 
   <van-empty v-if="userList === null || userList.length == 0" description="搜索结果为空"/>
 
@@ -26,23 +28,24 @@ import qs from 'qs';
 import myAxios from '../plugins/MyAxios';
 import {useRoute} from "vue-router";
 import {showToast} from "vant";
+import UserCardList from "../components/UserCardList.vue";
 
 const userList = ref([]);
 
-// mock测试数据
-// const userList = ref([{
-//   id: 1,
-//   username: '星河',
-//   userAccount: 'Leovany',
-//   profile: '一名程序猿，喜欢编码，巴拉巴拉巴巴拉巴拉巴巴，谢谢大家',
-//   avatarUrl: 'https://p3-passport.byteacctimg.com/img/user-avatar/c37ce093307b9b41d7197b041ad9be03~40x40.awebp',
-//   gender: '男',
-//   phone: '13000000000',
-//   email: '123@qq.com',
-//   planetCode: '123',
-//   tags: ['java', '打工中', '考研', '学习中'],
-//   createTime: new Date(),
-// }]);
+mock测试数据
+const userList = ref([{
+  id: 1,
+  username: '星河',
+  userAccount: 'Leovany',
+  profile: '一名程序猿，喜欢编码，巴拉巴拉巴巴拉巴拉巴巴，谢谢大家',
+  avatarUrl: 'https://p3-passport.byteacctimg.com/img/user-avatar/c37ce093307b9b41d7197b041ad9be03~40x40.awebp',
+  gender: '男',
+  phone: '13000000000',
+  email: '123@qq.com',
+  planetCode: '123',
+  tags: ['java', '打工中', '考研', '学习中'],
+  createTime: new Date(),
+}]);
 
 var route = useRoute();
 const tags = route.query.tags;
