f1cc4f8a6ee7cadcf8a5dd617b169fa9489504a4
Leovany <348083098@qq.com>
2023-12-19 23:18:02 +0800

    1

--- src/pages/SearchResultPage.vue
+++ src/pages/SearchResultPage.vue
@@ -32,20 +32,20 @@ import UserCardList from "../components/UserCardList.vue";
 
 const userList = ref([]);
 
-mock测试数据
-const userList = ref([{
-  id: 1,
-  username: '星河',
-  userAccount: 'Leovany',
-  profile: '一名程序猿，喜欢编码，巴拉巴拉巴巴拉巴拉巴巴，谢谢大家',
-  avatarUrl: 'https://p3-passport.byteacctimg.com/img/user-avatar/c37ce093307b9b41d7197b041ad9be03~40x40.awebp',
-  gender: '男',
-  phone: '13000000000',
-  email: '123@qq.com',
-  planetCode: '123',
-  tags: ['java', '打工中', '考研', '学习中'],
-  createTime: new Date(),
-}]);
+// mock测试数据
+// const userList = ref([{
+//   id: 1,
+//   username: '星河',
+//   userAccount: 'Leovany',
+//   profile: '一名程序猿，喜欢编码，巴拉巴拉巴巴拉巴拉巴巴，谢谢大家',
+//   avatarUrl: 'https://p3-passport.byteacctimg.com/img/user-avatar/c37ce093307b9b41d7197b041ad9be03~40x40.awebp',
+//   gender: '男',
+//   phone: '13000000000',
+//   email: '123@qq.com',
+//   planetCode: '123',
+//   tags: ['java', '打工中', '考研', '学习中'],
+//   createTime: new Date(),
+// }]);
 
 var route = useRoute();
 const tags = route.query.tags;
